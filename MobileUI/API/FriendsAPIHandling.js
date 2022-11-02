e4b424e3e9903bce42a864b4fba303844edcf5dd
hw <hopefully.purple@gmail.com>
2022-11-02 13:04:46 -0600

    GetReceivedRequests API method implemented

--- MobileUI/API/FriendsAPIHandling.js
+++ MobileUI/API/FriendsAPIHandling.js
@@ -61,6 +61,35 @@ export async function friendsGetSentRequests(userName) {
 }
 
 /**
+ * API call to get recieved requests of user
+ * If something goes wrong, catches error and goes to hardcoded functionality
+ * @returns json array of recieved friend requests
+ */
+export async function friendsGetRecievedRequests(userName) {
+  console.log('(FAPIHandling) Beginning of GetReceivedRequests');
+  let user = await getUsernameValue(userName);
+  try {
+    const response = await fetch(
+      'http://ec2-3-84-219-120.compute-1.amazonaws.com/FriendsModels/GetReceivedRequests',
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      },
+    );
+    const result = await response.json();
+
+    console.log(JSON.stringify(result, undefined, 2));
+    return result;
+  } catch (err) {
+    console.log('something went wrong with friendsGetReceivedRequests: ' + err);
+    // throw err;
+    return [];
+  }
+}
+
+/**
  * API call to create a friend request using email
  * If something goes wrong, catches error and goes to hardcoded functionality
  * @returns response.ok
