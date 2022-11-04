2600f91644086c2d8cf5ef5f31bb9e58e6ea077d
hw <hopefully.purple@gmail.com>
2022-11-04 17:40:17 -0600

    Collect letsmeet info and load for 3 secs when button pushed

--- MobileUI/screens/Groups/LetsMeetModal.js
+++ MobileUI/screens/Groups/LetsMeetModal.js
@@ -3,25 +3,103 @@ import {
   Text,
   StyleSheet,
   View,
+  ScrollView,
+  SafeAreaView,
   FlatList,
   TouchableOpacity,
   Button,
   Alert,
 } from 'react-native';
-import {SmallBoxButton} from '../../assets/components/CustomButtons';
+import {TextInput} from 'react-native-paper';
+import {BoxButton} from '../../assets/components/CustomButtons';
+import {Picker} from '@react-native-picker/picker';
 import Colors from '../../assets/styles/colors';
 
+const TIME_FRAMES = [
+  {value: 1, label: 'Within 1 day from now'},
+  {value: 2, label: 'Within 2 days from now'},
+  {value: 3, label: 'Within 3 days from now'},
+  {value: 4, label: 'Within 4 days from now'},
+  {value: 5, label: 'Within 5 days from now'},
+  {value: 6, label: 'Within 6 days from now'},
+  {value: 7, label: 'Within 7 days from now'},
+];
+
 export default function LetsMeetModal({navigation}) {
+  const [reason, setReason] = useState('');
+  const [location, setLocation] = useState('');
+  const [selectedTimeFrame, setSelectedTimeFrame] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  this.reasonInput = React.createRef();
+  this.locationInput = React.createRef();
+  // this.scrollView = React.createRef();
+
+  const handleLetsMeet = () => {
+    setIsLoading(true);
+    // this.scrollView.scrollToEnd({animated: true});
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  };
+
   return (
     <View style={styles.screenContainer}>
-      <View style={styles.innerContainer}>
+      <SafeAreaView style={styles.innerContainer}>
         <View style={styles.creamKnob} />
-        <View style={styles.bodyContainer}>
+        <ScrollView style={styles.bodyContainer}>
           <Text style={styles.infoText}>
             Enter info and we'll suggest good times to meet!
           </Text>
-        </View>
-      </View>
+          <TextInput
+            label="Reason for meeting"
+            value={reason}
+            onChangeText={text => setReason(text)}
+            mode="outlined"
+            style={styles.input}
+            activeOutlineColor={Colors.DD_RED_2}
+            autoCorrect={false}
+            ref={this.reasonInput}
+          />
+          <TextInput
+            label="Location"
+            value={location}
+            onChangeText={text => setLocation(text)}
+            mode="outlined"
+            style={styles.input}
+            activeOutlineColor={Colors.DD_RED_2}
+            autoCorrect={false}
+            ref={this.locationInput}
+          />
+          <Text style={styles.infoText}>
+            How soon do you want to hold the meeting?
+          </Text>
+          <Picker
+            selectedValue={selectedTimeFrame}
+            style={styles.timeFramePicker}
+            itemStyle={styles.timeFrameItem}
+            onValueChange={itemValue => {
+              setSelectedTimeFrame(itemValue);
+              // console.log(itemValue);
+            }}>
+            {TIME_FRAMES.map(item => (
+              <Picker.Item
+                key={item.value}
+                label={item.label}
+                value={item.value}
+              />
+            ))}
+          </Picker>
+          <Text style={styles.infoText}>Meeting duration: 1 hour</Text>
+          <View style={styles.button}>
+            <BoxButton title={`Let's Meet!`} onPress={handleLetsMeet} />
+            {!!isLoading && (
+              <Text style={styles.defaultScreentext}>Loading...</Text>
+            )}
+          </View>
+          <Text> </Text>
+        </ScrollView>
+      </SafeAreaView>
     </View>
   );
 }
@@ -36,6 +114,7 @@ const styles = StyleSheet.create({
     width: '100%',
     backgroundColor: Colors.DD_RED_2,
     borderRadius: 10,
+    paddingBottom: 10,
   },
   creamKnob: {
     alignSelf: 'center',
@@ -49,11 +128,32 @@ const styles = StyleSheet.create({
     height: '80%',
     backgroundColor: Colors.DD_CREAM,
     padding: 5,
+    // justifyContent: 'space-evenly',
   },
   infoText: {
     fontSize: 25,
-    fontWeight: 'bold',
+    // fontWeight: 'bold',
     color: Colors.DD_MEDIUM_GRAY,
-    paddingLeft: 5,
+    padding: 10,
+  },
+  input: {
+    margin: 10,
+  },
+  timeFramePicker: {
+    height: 100,
+    marginBottom: 100,
+  },
+  timeFrameItem: {
+    fontSize: 18,
+  },
+  button: {
+    alignItems: 'center',
+    // flexDirection: 'row',
+  },
+  defaultScreentext: {
+    fontSize: 25,
+    color: Colors.DD_RED_2,
+    textAlign: 'center',
+    margin: 20,
   },
 });
