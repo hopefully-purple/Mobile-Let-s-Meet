83a46b083328c8353e1920de453a43d1d96d709e
hw <hopefully.purple@gmail.com>
2022-11-26 20:53:34 -0600

    cleaned up profile

--- MobileUI/screens/ProfileScreen.js
+++ MobileUI/screens/ProfileScreen.js
@@ -5,35 +5,36 @@ import Colors from '../assets/styles/colors';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import UserContext from '../contexts/User';
 
+export default function ProfileScreen({navigation}) {
+  const user = useContext(UserContext);
+  return (
+    <SafeAreaView style={styles.screenContainer}>
+      <View style={styles.screenContainer}>
+        <Text style={styles.defaultScreentext}> Username: {user.name}</Text>
+        {/* <Text style={styles.defaultScreentext}> Password: {user.password}</Text> */}
+        {/* <Text style={styles.defaultScreentext}>
+          Swipe from the left to open navigation tool
+        </Text> */}
+      </View>
+    </SafeAreaView>
+  );
+}
+
 const styles = StyleSheet.create({
   screenContainer: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    // justifyContent: 'center',
     backgroundColor: Colors.DD_CREAM,
     color: Colors.DD_RED_2,
+    padding: 20,
   },
   defaultScreentext: {
     fontSize: 25,
     fontWeight: '500',
     color: Colors.DD_RED_2,
     textAlign: 'center',
+    margin: 10,
   },
 });
-
-export default function ProfileScreen({navigation}) {
-  const user = useContext(UserContext);
-  return (
-    <SafeAreaView style={styles.screenContainer}>
-      <View style={styles.screenContainer}>
-        <Text style={styles.defaultScreentext}> This is my profile</Text>
-        <Text style={styles.defaultScreentext}> Username: {user.name}</Text>
-        <Text style={styles.defaultScreentext}> Password: {user.password}</Text>
-        <Text style={styles.defaultScreentext}> QR code</Text>
-        <Text style={styles.defaultScreentext}>
-          Swipe from the left to open navigation tool
-        </Text>
-      </View>
-    </SafeAreaView>
-  );
-}
